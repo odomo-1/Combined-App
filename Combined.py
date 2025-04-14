0eb07aad2549083eedb530025f324d6dbb0b6a74
Omogoroye Odunayo <omogoroyeodunayo@gmail.com>
2025-04-14 13:35:48 +0100

    Update Combined.py

--- Combined.py
+++ Combined.py
@@ -11,7 +11,7 @@ from fuzzywuzzy import fuzz
 import fitz  # PyMuPDF
 from nltk.tokenize import sent_tokenize
 import nltk
-nltk.data.path.append("C:\Users\Sahel Consulting\OneDrive - Sahel Consulting\Documents\GitHub\Combined-App\nltk_data\tokenizers\punkt\english.pickle")
+nltk.data.path.append(r"C:\Users\Sahel Consulting\OneDrive - Sahel Consulting\Documents\GitHub\Combined-App\nltk_data")
 
 
 # --- Constants ---
