d98127a5a8a987e1809a3ecf91bfa2c90334613e
Omogoroye Odunayo <omogoroyeodunayo@gmail.com>
2025-04-14 16:41:41 +0100

    Update one.py

--- one.py
+++ one.py
@@ -1,9 +1,7 @@
 import nltk
 import os
 
-# Set the path for NLTK data
-nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
-nltk.data.path.append(nltk_data_path)
+import nltk
 
-# Download punkt if not already available
-nltk.download("punkt", download_dir=nltk_data_path)
+nltk.download('punkt')
+nltk.download('punkt_tab')
