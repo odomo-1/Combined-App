ff275fdd89bc89fd07eae32db2bc976fdc6420b9
Omogoroye Odunayo <omogoroyeodunayo@gmail.com>
2025-04-14 13:01:51 +0100

    Update Combined.py

--- Combined.py
+++ Combined.py
@@ -11,7 +11,7 @@ from fuzzywuzzy import fuzz
 import fitz  # PyMuPDF
 from nltk.tokenize import sent_tokenize
 import nltk
-nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
+nltk_data_path = os.path.join(os.getcwd(), "punkt")
 nltk.data.path.append(nltk_data_path)
 
 
