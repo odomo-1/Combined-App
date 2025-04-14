7b85e0b76a0db2ff6faa07505bec4ef40c9dc5ca
Omogoroye Odunayo <omogoroyeodunayo@gmail.com>
2025-04-14 16:33:40 +0100

    Update Combined.py

--- Combined.py
+++ Combined.py
@@ -16,9 +16,6 @@ import nltk
 nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
 nltk.data.path.append(nltk_data_path)
 
-# Download punkt if not already available
-nltk.download("punkt", download_dir=nltk_data_path)
-
 
 # --- Constants ---
 STANDARD_SECTIONS = [
